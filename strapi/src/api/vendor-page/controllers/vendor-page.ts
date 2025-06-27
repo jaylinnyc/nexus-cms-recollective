260b2303df924e1179638e89d53b46fbd9d1737e
Jay <jaylin8@gmail.com>
2025-06-27 00:02:55 -0400

    version 1 recollective content types

---
+++ strapi/src/api/vendor-page/controllers/vendor-page.ts
@@ -0,0 +1,7 @@
+/**
+ * vendor-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::vendor-page.vendor-page');
---
+++ strapi/src/api/vendor-page/routes/vendor-page.ts
@@ -0,0 +1,7 @@
+/**
+ * vendor-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::vendor-page.vendor-page');
